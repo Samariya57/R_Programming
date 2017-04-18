639e4a05e43dc1f62fa3c73d1425a3eb57b164e8
Mariia Danilenko <Samariya57@gmail.com>
2017-04-17 19:25:04 -0700

    Add files via upload

---
+++ Week4/rankhospital.R
@@ -0,0 +1,38 @@
+rankhospital <- function(state, outcome, num) {
+  
+  #Invalid outcome input type
+  if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
+    stop("invalid outcome")
+  }
+  
+  #Get index for our given outcome string.
+  index <- ifelse(outcome == "heart attack", 11, ifelse(outcome == "heart failure", 17, 23))
+  
+  
+  #Read and coerce our dataset while suppressing warnings and removing NA's.
+  data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+  data[,index] <- suppressWarnings(as.numeric(data[,index]))
+  data <- na.omit(data)
+  
+  
+  #Invalid state input or no observations
+  states <- table(data$State)
+  if (!state %in% names(states)) { 
+    stop("invalid state")
+  }
+  ##data<- data[order(data[2])]
+  #Slice our data by the given state and sort it by outcome and hospital name.
+  slice <- subset(data, data$State==state)
+  slice <- slice[order(slice[,index],slice[,2], na.last=TRUE),2]
+  slice <- na.omit(slice)
+  
+  switch(num, best = {
+    num = 1
+  }, worst = {
+    num = length(slice)
+  })
+  if (num > length(slice)) {
+    return(NA)
+  }
+  slice[num]
+}
